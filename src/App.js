f4fcaa369b6d415648b6904878d4f1339e52eb61
Pike <havelka.pvl@seznam.cz>
2023-04-16 15:05:54 +0200

    vsechno karel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import TheHeader from "./components/TheHeader"
+import TheMain from "./components/TheMain"
+import TheFooter from "./components/TheFooter"
+import { useRef } from "react"
 
 function App() {
+  const artGalleryRef = useRef(null)
+
+  // Scroll To Gallery from NavigationBar
+    const scrollToGallery = (e) => {
+    e.preventDefault()
+
+    window.scrollTo({
+      top: artGalleryRef.current.offsetTop,
+      behavior: "smooth"
+    })
+  }
+
   return (
-    <>
-       změna obsahu
-    </>
+    <div className="container">
+      <TheHeader scrollToGallery={scrollToGallery}/>
+      <TheMain ref={artGalleryRef} />
+      <TheFooter />
+    </div>
   )
 }
 
